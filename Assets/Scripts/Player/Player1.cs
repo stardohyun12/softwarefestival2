7d9aaea00368ca7bef7f62d242afe16d809770d6
stardohyun <stardohyun11@gmail.com>
2023-12-20 00:23:00 +0900

    12/20-1

--- Assets/Scripts/Player/Player1.cs
+++ Assets/Scripts/Player/Player1.cs
@@ -4,19 +4,111 @@ using UnityEngine;
 
 public class Player1 : MonoBehaviour
 {
+    //캐릭터 가져오기
     public GameObject Fire;
     public GameObject Ground;
     public GameObject Metal;
-    public int CharacterType = 0;
+    //플레이1 스폰포인트
+    public Transform Player1Spown;
+    //캐릭터 rigid 가져오기
+    Rigidbody2D rigid;
+    Rigidbody2D Fire_rigid;
+    Rigidbody2D Ground_rigid;
+    Rigidbody2D Metal_rigid;
+
+    public FireKight Fire_C { get; private set; }
+
+    //플레이어 캐릭터 선택
+    public int Player1CharacterType = 0;
+
+    //입력-움직임
+    int playerInput = 0;
+
+    //Fire-수치
+    float Fire_MaxSpeed = 5f;
+
+    //Ground-수치
+    float Ground_MaxSpeed = 7f;
+
+    //Metal-수치
+    float Metal_MaxSpeed = 6.5f;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Fire_rigid = Fire.GetComponent<Rigidbody2D>();
+        Ground_rigid = Ground.GetComponent<Rigidbody2D>();
+        Metal_rigid = Metal.GetComponent<Rigidbody2D>();
+        
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Player1CharacterType == 1)
+        {
+            Instantiate(Fire,Player1Spown);
+        }
+        if(Player1CharacterType == 2)
+        {
+            Instantiate(Ground, Player1Spown);
+        }
+        if(Player1CharacterType == 3) 
+        {
+            Instantiate(Metal, Player1Spown);
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //기본 키 입력
+        if (Input.GetKeyDown(KeyCode.A))
+            playerInput = -1;
+        else if (Input.GetKeyDown(KeyCode.D))
+            playerInput = 1;
+
+        //Fire 캐릭터
+        if (Player1CharacterType == 1) 
+        {
+            //기본이동,최고속도
+            Fire_rigid.AddForce(Vector2.right * playerInput, ForceMode2D.Impulse);
+            if (Fire_rigid.velocity.x > Fire_MaxSpeed)//왼쪽 MaxSpeed
+                Fire_rigid.velocity = new Vector2(Fire_MaxSpeed, Fire_rigid.velocity.y);
+            else if (Fire_rigid.velocity.x < Fire_MaxSpeed*(-1))//오른쪽 MaxSpeed
+                Fire_rigid.velocity = new Vector2(Fire_MaxSpeed* (-1), Fire_rigid.velocity.y);
+
+            //애니메이션
+            if(playerInput != 0)
+            {
+                
+            }
+            
+
+
+
+        }
+        //Ground 캐릭터
+        else if (Player1CharacterType == 2)
+        {
+            //기본이동,최고속도
+            Ground_rigid.AddForce(Vector2.right * playerInput, ForceMode2D.Impulse);
+            if (Ground_rigid.velocity.x > Ground_MaxSpeed)//왼쪽 MaxSpeed
+                Ground_rigid.velocity = new Vector2(Ground_MaxSpeed, Ground_rigid.velocity.y);
+            else if (Ground_rigid.velocity.x < Ground_MaxSpeed * (-1))//오른쪽 MaxSpeed
+                Ground_rigid.velocity = new Vector2(Ground_MaxSpeed * (-1), Ground_rigid.velocity.y);
+        }
+        //Metal 캐릭터 
+        else if(Player1CharacterType == 3) 
+        {
+            //기본이동,최고속도
+            Metal_rigid.AddForce(Vector2.right * playerInput, ForceMode2D.Impulse);
+            if (Metal_rigid.velocity.x > Ground_MaxSpeed)//왼쪽 MaxSpeed
+                Metal_rigid.velocity = new Vector2(Ground_MaxSpeed, Metal_rigid.velocity.y);
+            else if (Metal_rigid.velocity.x < Ground_MaxSpeed * (-1))//오른쪽 MaxSpeed
+                Metal_rigid.velocity = new Vector2(Ground_MaxSpeed * (-1), Metal_rigid.velocity.y);
+        }
+
+
     }
 }
